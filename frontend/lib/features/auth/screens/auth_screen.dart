c5e0a386fdc03c132310fa4eb4b085bd69285cef
mohammadammash <mohammadammash31@gmail.com>
2022-12-07 11:55:45 +0200

    frontend/lib/features/auth/screens/auth_screen.dart: Adding auth radio buttons

--- frontend/lib/features/auth/screens/auth_screen.dart
+++ frontend/lib/features/auth/screens/auth_screen.dart
@@ -1,6 +1,9 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
+//Radio button values
+enum Auth { signin, signup }
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
@@ -9,6 +12,8 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.signup; //group value by default //State //radio input val
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,14 +22,48 @@ class _AuthScreenState extends State<AuthScreen> {
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
-            children: const [
-              Text('Welcome',
+            children: [
+              const Text('Welcome',
                   style: TextStyle(
                     fontSize: GlobalVariables.textXL,
                     fontWeight: FontWeight.w500,
                   )),
               ListTile(
-              )
+                title: const Text(
+                  'Create Account',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signup,
+                  groupValue: _auth,
+                  onChanged: (Auth? value) {
+                    setState(() {
+                      _auth = value!;
+                    });
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text(
+                  'Sign-in',
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                leading: Radio(
+                  activeColor: GlobalVariables.secondaryColor,
+                  value: Auth.signin,
+                  groupValue: _auth,
+                  onChanged: (Auth? value) {
+                    setState(() {
+                      _auth = value!;
+                    });
+                  },
+                ),
+              ),
             ],
           ),
         ),
