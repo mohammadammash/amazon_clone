491e5ef7ae28a25c3681bebf32be90b4220c995a
mohammadammash <mohammadammash31@gmail.com>
2022-12-07 11:20:20 +0200

    frontend/lib/utils/helpers/router.dart: Adding routers navigations handler file

---
+++ frontend/lib/utils/helpers/router.dart
@@ -0,0 +1,22 @@
+import 'package:amazon_clone/constants/routes_names.dart';
+import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import "package:flutter/material.dart";
+
+Route<dynamic> generateRoute(RouteSettings settings) {
+  switch (settings.name) {
+    case RoutesNames.authScreen:
+      return MaterialPageRoute(
+        builder: (context) => const AuthScreen(),
+      );
+
+    //default (error) if not route name is matched
+    default:
+      return MaterialPageRoute(
+        builder: (context) => const Scaffold(
+          body: Center(
+            child: Text('Screen Not Found!! 404'),
+          ),
+        ),
+      );
+  }
+}
