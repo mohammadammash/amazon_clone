cbe7152bb700e71078e4540c255d66ad43d2e3f0
mohammadammash <mohammadammash31@gmail.com>
2022-12-07 17:30:25 +0200

    backend: Adding .env and .env.example to use server port

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,7 @@
-console.log("Hellloooooo");
+const express = require("express");
+const app = express();
+require("dotenv").config();
+
+app.listen(process.env.SERVER_PORT, () => {
+  console.log("Running on " + process.env.SERVER_PORT);
+});
