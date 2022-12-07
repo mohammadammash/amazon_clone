beb3fd89e6574d275cc919a4d9a72c6cd5f4e14e
mohammadammash <mohammadammash31@gmail.com>
2022-12-07 13:13:13 +0200

    frontend/lib/features/common/widgets/text_input.dart: Adding placeholder text

--- frontend/lib/features/common/widgets/text_input.dart
+++ frontend/lib/features/common/widgets/text_input.dart
@@ -3,16 +3,22 @@ import 'package:flutter/material.dart';
 class CustomTextField extends StatelessWidget {
   //params
   final TextEditingController controller;
+  final String textPlaceholder;
 
-  const CustomTextField({super.key, required this.controller});
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.textPlaceholder,
+  });
 
   @override
   Widget build(BuildContext context) {
     //TextFormField instead of TextForm to use validator arg
     return TextFormField(
       controller: controller,
-      decoration: const InputDecoration(
-        border: OutlineInputBorder(
+      decoration: InputDecoration(
+        hintText: textPlaceholder,
+        border: const OutlineInputBorder(
           borderSide: BorderSide(
             color: Colors.black38,
           ),
