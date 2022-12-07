48fed84d37c6d216a24b1831a18ed5a52a44f50f
mohammadammash <mohammadammash31@gmail.com>
2022-12-07 11:37:09 +0200

    frontend/lib/constants/global_variables.dart: Adding textXL fixed typography

--- frontend/lib/constants/global_variables.dart
+++ frontend/lib/constants/global_variables.dart
@@ -13,10 +13,17 @@ class GlobalVariables {
 
   static const secondaryColor = Color.fromRGBO(255, 153, 0, 1);
   static const backgroundColor = Colors.white;
-  static const Color greyBackgroundCOlor = Color(0xffebecee);
+  static const Color greyBackgroundColor = Color(0xffebecee);
   static var selectedNavBarColor = Colors.cyan[800]!;
   static const unselectedNavBarColor = Colors.black87;
   // END OF COLORS
   //----------------
+
+  //-------------------
+  //START OF TYPOGRAPHY
+  static const double textXL = 22;
+
+  //END OF TYPOGRAPHY
+  //-------------------
 
 }
