6990d5916bd86760f6ce621cf57656d99856e89d
mohammadammash <mohammadammash31@gmail.com>
2022-12-07 13:13:28 +0200

    frontend/lib/features/common/widgets/button.dart: Creating Custom Button Widget

---
+++ frontend/lib/features/common/widgets/button.dart
@@ -0,0 +1,32 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback handlePress;
+
+  const CustomButton({
+    super.key,
+    required this.text,
+    required this.handlePress,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        child: Text(
+          text.toUpperCase(),
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            fontSize: GlobalVariables.textLG,
+          ),
+        ),
+        onPressed: handlePress,
+        style: ElevatedButton.styleFrom(
+          minimumSize: const Size(
+            double.infinity,
+            50,
+          ),
+        ));
+  }
+}
