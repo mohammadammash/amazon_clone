29501d33ae0b86e3441a4a0731f8794eaa8b8f95
mohammadammash <mohammadammash31@gmail.com>
2022-12-08 08:26:13 +0200

    backend/src/index.js: Adding middleware for authRoutes

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
+const authRoutes = require("./routes/auth");
+app.use("/api", authRoutes);
+
 app.listen(process.env.SERVER_PORT, () => {
   console.log("Running on " + process.env.SERVER_PORT);
 });
