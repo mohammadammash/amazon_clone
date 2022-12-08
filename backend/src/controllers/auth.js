df8284d5c13740e2edc1fccf00b0ca11a4685353
mohammadammash <mohammadammash31@gmail.com>
2022-12-08 09:11:00 +0200

    backend/src/controllers/auth.js: Adding registerUser controller functionality

--- backend/src/controllers/auth.js
+++ backend/src/controllers/auth.js
@@ -1,5 +1,27 @@
+const bcryptjs = require("bcryptjs");
+
 const registerUserController = async (req, res) => {
-  res.json({ message: "Hello" });
+  try {
+    const { name, email, password } = req.body;
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(400).json({ msg: "User with same email already exists!" });
+    }
+
+    const hashedPassword = await bcryptjs.hash(password, 8);
+
+    let user = new User({
+      email,
+      password: hashedPassword,
+      name,
+    });
+
+    user = await user.save();
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
 };
 
 const loginUserController = async (req, res) => {
