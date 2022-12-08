12bc5f63a13a47fc9b04d5586feab09d4d8ce5b9
mohammadammash <mohammadammash31@gmail.com>
2022-12-08 11:43:36 +0200

    frontend/lib/utils/response_handle.dart: Adding void function to handle all response type and what to show to user

---
+++ frontend/lib/utils/response_handle.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:amazon_clone/utils/snackbar.dart';
+import 'package:flutter/cupertino.dart';
+import "package:http/http.dart" as http;
+
+void httpResponseHandle({
+  required http.Response response,
+  required BuildContext context,
+  required VoidCallback onSuccess,
+}) {
+  switch (response.statusCode) {
+    case 200:
+      onSuccess();
+      break;
+
+    case 400:
+      showSnackBar(
+        context,
+        jsonDecode(response.body)['msg'],
+      ); //message as sent in json from backend ({'msg':...})
+      break;
+
+    case 500:
+      showSnackBar(
+          context,
+          jsonDecode(response.body)[
+              'error']); //error as sent in json from backend ({'error':...})
+      break;
+
+    default:
+      showSnackBar(context, "Something went wrong");
+      break;
+  }
+}
