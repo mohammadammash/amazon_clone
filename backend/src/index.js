e72aa27a1c908298893145a7071418ea2924d663
mohammadammash <mohammadammash31@gmail.com>
2022-12-08 14:18:02 +0200

    backend/src/index.js: Adding bodyParser and cors packages

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
 require("dotenv").config();
 require("./database/connection");
 
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 const authRoutes = require("./routes/auth");
 app.use("/api", authRoutes);
