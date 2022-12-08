ee96737c753ca16aaabcff6625e6a66006a0d3dd
mohammadammash <mohammadammash31@gmail.com>
2022-12-08 11:52:09 +0200

    frontend/lib/utils: Fixing imports

--- frontend/lib/utils/response_handle.dart
+++ frontend/lib/utils/response_handle.dart
@@ -1,7 +1,6 @@
 import 'dart:convert';
-
 import 'package:amazon_clone/utils/snackbar.dart';
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import "package:http/http.dart" as http;
 
 void httpResponseHandle({
