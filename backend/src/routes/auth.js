5af09706efade05700f56e0db995402d5332c147
mohammadammash <mohammadammash31@gmail.com>
2022-12-09 19:36:10 +0200

    backend/src/routes/auth.js: Adding getUserData route

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const router = express.Router();
 //internal
-const { loginUserController, registerUserController } = require("../controllers/auth");
+const { loginUserController, registerUserController, getUserDataController } = require("../controllers/auth");
+const authMiddleware = require("../middlewares/auth");
 
+router.get("/user", authMiddleware, getUserDataController);
 router.post("/login", loginUserController);
 router.post("/register", registerUserController);
 
