e706d67466ce2de14d182955ef27bfae247ee5f9
mohammadammash <mohammadammash31@gmail.com>
2022-12-09 12:26:54 +0200

    frontend/lib/utils/router.dart: Adding home screen route to default main scaffold route navigator

--- frontend/lib/utils/router.dart
+++ frontend/lib/utils/router.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone/constants/routes_names.dart';
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/features/home/screens/home_screen.dart';
 import "package:flutter/material.dart";
 
 Route<dynamic> generateRoute(RouteSettings settings) {
@@ -7,6 +8,11 @@ Route<dynamic> generateRoute(RouteSettings settings) {
     case RoutesNames.authScreen:
       return MaterialPageRoute(
         builder: (context) => const AuthScreen(),
+      );
+
+    case RoutesNames.homeScreen:
+      return MaterialPageRoute(
+        builder: (context) => const HomeScreen(),
       );
 
     //default (error) if not route name is matched
