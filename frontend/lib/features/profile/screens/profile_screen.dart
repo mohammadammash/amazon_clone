ca4a2fb7519b643ed81f606353e886ed722371ea
mohammadammash <mohammadammash31@gmail.com>
2022-12-10 12:25:17 +0200

    frontend/lib/features/profile/screens/profile_screen.dart: Adding top buttons widgets to body

--- frontend/lib/features/profile/screens/profile_screen.dart
+++ frontend/lib/features/profile/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/profile/widgets/below_appbar_header.dart';
+import 'package:amazon_clone/features/profile/widgets/profile_button.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -13,51 +14,70 @@ class _ProfileScreenState extends State<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        //-------------------
-        //APP BAR
-        appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(50),
-          child: AppBar(
-            flexibleSpace: Container(
-              decoration: const BoxDecoration(
-                gradient: GlobalVariables.appBarGradient,
-              ),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Container(
-                  alignment: Alignment.topLeft,
-                  child: Image.asset(
-                    'lib/assets/images/amazon_in.png',
-                    width: 120,
-                    height: 45,
-                    color: Colors.black,
-                  ),
-                ),
-                Container(
-                  padding: const EdgeInsets.only(left: 15, right: 15),
-                  child: Row(
-                    children: const [
-                      Padding(
-                        padding: EdgeInsets.only(right: 15),
-                        child: Icon(Icons.notifications_outlined),
-                      ),
-                      Icon(
-                        Icons.search,
-                      ),
-                    ],
-                  ),
-                )
-              ],
+      //-------------------
+      //APP BAR
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(50),
+        child: AppBar(
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: GlobalVariables.appBarGradient,
             ),
           ),
+          title: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                alignment: Alignment.topLeft,
+                child: Image.asset(
+                  'lib/assets/images/amazon_in.png',
+                  width: 120,
+                  height: 45,
+                  color: Colors.black,
+                ),
+              ),
+              Container(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Row(
+                  children: const [
+                    Padding(
+                      padding: EdgeInsets.only(right: 15),
+                      child: Icon(Icons.notifications_outlined),
+                    ),
+                    Icon(
+                      Icons.search,
+                    ),
+                  ],
+                ),
+              )
+            ],
+          ),
         ),
-        body: Column(
-          children: const [
-            BelowAppBarHeader(),
-            SizedBox(height: 10),
-          ],
-        ));
+      ),
+      body: Column(
+        children: [
+          const BelowAppBarHeader(),
+          const SizedBox(height: 10),
+          Column(
+            children: [
+              Row(
+                children: [
+                  ProfileButton(text: 'Your orders', handlePress: () {}),
+                  ProfileButton(text: 'Turn to Seller', handlePress: () {}),
+                ],
+              ),
+              const SizedBox(height: 10),
+              Row(
+                children: [
+                  ProfileButton(text: 'Log out', handlePress: () {}),
+                  ProfileButton(text: 'Your Wishlist', handlePress: () {}),
+                ],
+              )
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
+    );
   }
 }
