ee355da75b5207c18cb92ae64483b8e77e480654
mohammadammash <mohammadammash31@gmail.com>
2022-12-11 09:19:32 +0200

    frontend/lib/constants/data_lists.dart: Adding categories data to constants

---
+++ frontend/lib/constants/data_lists.dart
@@ -0,0 +1,24 @@
+class ConstantDataLists {
+  static const List<Map<String, String>> homeCategoriesData = [
+    {
+      'title': 'Mobiles',
+      'image': 'lib/assets/images/mobiles.jpeg',
+    },
+    {
+      'title': 'Essentials',
+      'image': 'lib/assets/images/essentials.jpeg',
+    },
+    {
+      'title': 'Appliances',
+      'image': 'lib/assets/images/appliances.jpeg',
+    },
+    {
+      'title': 'Books',
+      'image': 'lib/assets/images/books.jpeg',
+    },
+    {
+      'title': 'Fashion',
+      'image': 'lib/assets/images/fashion.jpeg',
+    },
+  ];
+}
