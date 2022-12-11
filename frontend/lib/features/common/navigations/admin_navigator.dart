399594b9cb263e09eee86d4402d50c01c98cda8b
mohammadammash <mohammadammash31@gmail.com>
2022-12-11 20:46:33 +0200

    frontend/lib/features/common/navigations/admin_navigator.dart: Adding common custom app bar

--- frontend/lib/features/common/navigations/admin_navigator.dart
+++ frontend/lib/features/common/navigations/admin_navigator.dart
@@ -1,15 +1,27 @@
+import 'package:amazon_clone/features/common/widgets/app_bar.dart';
+import 'package:amazon_clone/features/common/widgets/bottom_bar.dart';
+import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
-class AdminBottomNavbar extends StatefulWidget {
-  const AdminBottomNavbar({super.key});
+class AdminNavigator extends StatefulWidget {
+  const AdminNavigator({super.key});
 
   @override
-  State<AdminBottomNavbar> createState() => _AdminBottomNavbarState();
+  State<AdminNavigator> createState() => _AdminNavigatorState();
 }
 
-class _AdminBottomNavbarState extends State<AdminBottomNavbar> {
+class _AdminNavigatorState extends State<AdminNavigator> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    final currentUser = Provider.of<UserProvider>(context).user;
+
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: const Size.fromHeight(60),
+        child: CustomAppBar(currentUserType: currentUser.type),
+      ),
+      bottomNavigationBar: CustomBottomBar(currentPage: currentPage, updatePage: updatePage),
+    );
   }
 }
