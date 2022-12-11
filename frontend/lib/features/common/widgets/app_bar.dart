3905b8b3b90c043ab8009615be83f97924f68410
mohammadammash <mohammadammash31@gmail.com>
2022-12-11 20:42:53 +0200

    frontend/lib/features/common/widgets/app_bar.dart: Adding admin conditional app bar

--- frontend/lib/features/common/widgets/app_bar.dart
+++ frontend/lib/features/common/widgets/app_bar.dart
@@ -16,56 +16,64 @@ class CustomAppBar extends StatelessWidget {
       title: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
-            child: Container(
-              height: 42,
-              margin: const EdgeInsets.only(left: 15),
-              child: Material(
-                borderRadius: BorderRadius.circular(7),
-                elevation: 1,
-                child: TextFormField(
-                  decoration: InputDecoration(
-                    prefixIcon: InkWell(
-                      onTap: () {},
-                      child: const Padding(
-                        padding: EdgeInsets.only(
-                          left: 6,
+          if (currentUserType != GlobalVariables.adminUserType)
+            Expanded(
+              child: Container(
+                height: 42,
+                margin: const EdgeInsets.only(left: 15),
+                child: Material(
+                  borderRadius: BorderRadius.circular(7),
+                  elevation: 1,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      prefixIcon: InkWell(
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.only(
+                            left: 6,
+                          ),
+                          child: Icon(
+                            Icons.search,
+                            color: Colors.black,
+                            size: 23,
+                          ),
                         ),
-                        child: Icon(
-                          Icons.search,
-                          color: Colors.black,
-                          size: 23,
+                      ),
+                      filled: true,
+                      fillColor: Colors.white,
+                      contentPadding: const EdgeInsets.only(top: 10),
+                      border: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(7),
+                        ),
+                        borderSide: BorderSide.none,
+                      ),
+                      enabledBorder: const OutlineInputBorder(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(7),
+                        ),
+                        borderSide: BorderSide(
+                          color: Colors.black38,
+                          width: 1,
                         ),
                       ),
-                    ),
-                    filled: true,
-                    fillColor: Colors.white,
-                    contentPadding: const EdgeInsets.only(top: 10),
-                    border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(7),
+                      hintText: 'Search Amazon.in',
+                      hintStyle: const TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 17,
                       ),
-                      borderSide: BorderSide.none,
-                    ),
-                    enabledBorder: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(7),
-                      ),
-                      borderSide: BorderSide(
-                        color: Colors.black38,
-                        width: 1,
-                      ),
-                    ),
-                    hintText: 'Search Amazon.in',
-                    hintStyle: const TextStyle(
-                      fontWeight: FontWeight.w500,
-                      fontSize: 17,
                     ),
                   ),
                 ),
               ),
+            )
+          else
+            Image.asset(
+              'lib/assets/images/amazon_in.png',
+              width: 120,
+              height: 50,
+              color: Colors.black,
             ),
-          ),
           if (currentUserType != GlobalVariables.adminUserType)
             Container(
               padding: const EdgeInsets.only(left: 15, right: 15),
