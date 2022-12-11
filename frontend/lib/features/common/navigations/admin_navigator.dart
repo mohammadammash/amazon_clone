cd1530d5f1586a7046978b4b465a6fe388543de6
mohammadammash <mohammadammash31@gmail.com>
2022-12-11 19:52:04 +0200

    frontend/lib/features/common/navigations/admin_navigator.dart: Adding basic admin navigator

---
+++ frontend/lib/features/common/navigations/admin_navigator.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AdminBottomNavbar extends StatefulWidget {
+  const AdminBottomNavbar({super.key});
+
+  @override
+  State<AdminBottomNavbar> createState() => _AdminBottomNavbarState();
+}
+
+class _AdminBottomNavbarState extends State<AdminBottomNavbar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
