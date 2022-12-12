16df316af25f7d407e10e288983c35262f99973e
mohammadammash <mohammadammash31@gmail.com>
2022-12-12 17:58:40 +0200

    frontend/lib/features/admin/screens/add_product_screen.dart: Installing dottedBorder package and adding input image border input

--- frontend/lib/features/admin/screens/add_product_screen.dart
+++ frontend/lib/features/admin/screens/add_product_screen.dart
@@ -1,5 +1,6 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/common/widgets/app_bar.dart';
+import 'package:dotted_border/dotted_border.dart';
 import 'package:flutter/material.dart';
 
 class AddProductScreen extends StatefulWidget {
@@ -12,15 +13,51 @@ class AddProductScreen extends StatefulWidget {
 class _AddProductScreenState extends State<AddProductScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-        appBar: PreferredSize(
+    return Scaffold(
+        appBar: const PreferredSize(
           preferredSize: Size.fromHeight(60),
           child: CustomAppBar(
               currentUserType: GlobalVariables.adminUserType,
               title: 'Add Product'),
         ),
-        body: Center(
-          child: Text('Add New Product'),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 15.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 20),
+                DottedBorder(
+                  //external package
+                  borderType: BorderType.RRect,
+                  radius: const Radius.circular(10),
+                  dashPattern: const [10, 4],
+                  strokeCap: StrokeCap.round,
+                  strokeWidth: 2,
+                  child: Container(
+                    //dotted container
+                    width: double.infinity,
+                    height: 150,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      //dotted container data
+                      children: [
+                        const Icon(Icons.folder_open, size: 40),
+                        const SizedBox(height: 10),
+                        Text('Select Product Images',
+                            style: TextStyle(
+                              fontSize: GlobalVariables.textSM,
+                              color: Colors.grey.shade500,
+                            ))
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
         ));
   }
 }
