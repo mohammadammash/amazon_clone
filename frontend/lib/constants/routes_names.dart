d332432e03bb0e87102f7ef3cba84f0c58964aa8
mohammadammash <mohammadammash31@gmail.com>
2022-12-12 09:36:15 +0200

    frontend/lib/constants/routes_names.dart: Adding product screen route name

--- frontend/lib/constants/routes_names.dart
+++ frontend/lib/constants/routes_names.dart
@@ -1,5 +1,18 @@
 class RoutesNames {
   static const authScreen = 'auth_screen';
+
+  //-------------------------------------
+  //START OF USER SCREENS
   static const homeScreen = 'home_screen';
   static const userHomeBottomNavigation = 'home_navigation';
+  //END OF USER SCREENS
+  //-------------------------------------
+
+  //-------------------------------------
+  //START OF ADMIN SCREENS
+  static const postsScreen = 'posts_screen';
+  static const addProductScreen = 'add_product_screen';
+  //END OF USER SCREENS
+  //-------------------------------------
+
 }
