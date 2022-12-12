40444808613dd407810bc611ea4ea45aa8bdbf5c
mohammadammash <mohammadammash31@gmail.com>
2022-12-12 09:10:11 +0200

    frontend/lib/features/admin/screens/posts_sreen.dart: Adding basic start screen

---
+++ frontend/lib/features/admin/screens/posts_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class PostsScreen extends StatefulWidget {
+  const PostsScreen({super.key});
+
+  @override
+  State<PostsScreen> createState() => _PostsScreenState();
+}
+
+class _PostsScreenState extends State<PostsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: const Center(
+        child: Text('Products'),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
