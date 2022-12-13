f2762cf64f98d2466b0ff6342e06b8dd4c931266
mohammadammash <mohammadammash31@gmail.com>
2022-12-13 11:25:49 +0200

    backend/src/index.js: Adding admin routes and middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,8 +10,15 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+//ROUTES
+const authMiddleware = require("./middlewares/auth");
+const checkIfAdminMiddleware = require("./middlewares/checkIfAdmin");
+
 const authRoutes = require("./routes/auth");
 app.use("/api", authRoutes);
+
+const adminRoutes = require("./routes/admin");
+app.use("/admin", authMiddleware, checkIfAdminMiddleware, adminRoutes);
 
 app.listen(process.env.SERVER_PORT, () => {
   console.log("Running on " + process.env.SERVER_PORT);
