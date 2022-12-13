90d79c19fc231a317a78beb9658e3ff9576941df
mohammadammash <mohammadammash31@gmail.com>
2022-12-13 09:29:38 +0200

    frontend/lib/features/common/widgets/carousel_image.dart: Extracting carousel to common and run it conditionally

---
+++ frontend/lib/features/common/widgets/carousel_image.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:amazon_clone/constants/data_lists.dart';
+import 'package:flutter/material.dart';
+import 'package:carousel_slider/carousel_slider.dart';
+
+class CarouselImage extends StatefulWidget {
+  final List<File>? data;
+  const CarouselImage({super.key, this.data});
+
+  @override
+  State<CarouselImage> createState() => _CarouselImageState();
+}
+
+class _CarouselImageState extends State<CarouselImage> {
+  @override
+  Widget build(BuildContext context) {
+    return CarouselSlider(
+      items: widget.data != null
+          ? widget.data?.map((File item) {
+              return Builder(
+                builder: (BuildContext context) =>
+                    Image.file(item, fit: BoxFit.cover, height: 200),
+              );
+            }).toList()
+          : ConstantDataLists.carouselImages.map((item) {
+              return Builder(
+                builder: (BuildContext context) =>
+                    Image.network(item, fit: BoxFit.cover, height: 200),
+              );
+            }).toList(),
+      options: CarouselOptions(
+        viewportFraction: 1,
+      ),
+    );
+  }
+}
