8d1624fcf9c4a9e471260064eda25213ca8099ee
mohammadammash <mohammadammash31@gmail.com>
2022-12-13 11:24:01 +0200

    frontend/lib/constants/config.dart: Installing cloudinary_public library and adding cloudinary config data

---
+++ frontend/lib/constants/config.dart
@@ -0,0 +1,4 @@
+class ConstantConfigData {
+  static const cloudinaryName = 'du2uvywzb'; //dashboard
+  static const cloudinaryUploadPreset ='sfydlgbj'; //settings -> upload -> Enable unsigned uploading -> copy name of signed upload presetsfydlgbj
+}
