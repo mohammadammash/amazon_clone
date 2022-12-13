593d19edbecf35b537d84dff4ed511d0299c9989
mohammadammash <mohammadammash31@gmail.com>
2022-12-13 11:23:02 +0200

    frontend/lib/models/product.dart: Creating product class model

---
+++ frontend/lib/models/product.dart
@@ -0,0 +1,53 @@
+import 'dart:convert';
+
+class Product {
+  final String name;
+  final String description;
+  final double quantity;
+  final List<String> images; //string as urls
+  final String category;
+  final double price;
+  String? id; //optional to add according to response
+  String? userId;
+
+  Product({
+    required this.name,
+    required this.description,
+    required this.quantity,
+    required this.images,
+    required this.category,
+    required this.price,
+    this.id,
+    this.userId,
+  });
+
+    Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'description': description,
+      'quantity': quantity,
+      'images': images,
+      'category': category,
+      'price': price,
+      'id': id,
+      'userId': userId,
+    };
+  }
+
+  factory Product.fromMap(Map<String, dynamic> map) {
+    return Product(
+      name: map['name'] ?? '',
+      description: map['description'] ?? '',
+      quantity: map['quantity'] ?? '',
+      images: map['images'] ?? '',
+      category: map['category'] ?? '',
+      price: map['price'] ?? '',
+      id: map['_id'] ?? '', //_id: since we're using mongoDB _id
+      userId: map['userId'] ?? '',
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory Product.fromJson(String source) => Product.fromMap(json.decode(source));
+}
