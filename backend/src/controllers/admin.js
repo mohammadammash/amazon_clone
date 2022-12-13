46f52efa99c63dbfc6a215127fa7660e58932005
mohammadammash <mohammadammash31@gmail.com>
2022-12-13 12:45:00 +0200

    backend/src/controllers/admin.js: Adding basic testing getAllProducts Controller

--- backend/src/controllers/admin.js
+++ backend/src/controllers/admin.js
@@ -3,7 +3,7 @@ const Product = require("../database/models/product");
 const addProductController = async (req, res) => {
   try {
     const { name, description, images, quantity, price, category } = req.body;
-    
+
     const product = new Product({
       name,
       description,
@@ -20,6 +20,15 @@ const addProductController = async (req, res) => {
   }
 };
 
+const getAllProductsController = async (req, res) => {
+  try {
+    res.status(200).json({ msg: "get all productsooooo" });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
 module.exports = {
   addProductController,
+  getAllProductsController,
 };
