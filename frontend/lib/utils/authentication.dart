cc9052e1e632ef6139e6d70ffde7775eff18ab54
mohammadammash <mohammadammash31@gmail.com>
2022-12-14 11:43:46 +0200

    frontend/lib/utils/authentication.dart: Adding common api headers

--- frontend/lib/utils/authentication.dart
+++ frontend/lib/utils/authentication.dart
@@ -3,10 +3,19 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 class Authentication {
-  
   String getAuthToken({
     required BuildContext context,
   }) {
     return Provider.of<UserProvider>(context, listen: false).user.token;
+  }
+
+  Map<String, String> getAPIsHeader({
+    required BuildContext context,
+  }) {
+    return {
+      //to match express.json() in express server
+      'Content-Type': 'application/json; charset=UTF-8',
+      'x-auth-token': getAuthToken(context: context),
+    };
   }
 }
