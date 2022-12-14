01fb82913e83249c7ef9b6748b7c3e65e34fffd5
mohammadammash <mohammadammash31@gmail.com>
2022-12-14 21:35:32 +0200

    frontend/lib/features/home/widgets/top_categories.dart: Adding param to handle Category Press

--- frontend/lib/features/home/widgets/top_categories.dart
+++ frontend/lib/features/home/widgets/top_categories.dart
@@ -3,7 +3,8 @@ import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 class TopCategories extends StatelessWidget {
-  const TopCategories({super.key});
+  final Function(String) handleOnPressCategory;
+  const TopCategories({super.key, required this.handleOnPressCategory});
 
   @override
   Widget build(BuildContext context) {
@@ -12,33 +13,39 @@ class TopCategories extends StatelessWidget {
       child: ListView.builder(
         itemCount: ConstantDataLists.homeCategoriesData.length,
         scrollDirection: Axis.horizontal,
-        itemExtent: 95,
+        itemExtent: 83,
         itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(50),
-                  child: Image.asset(
-                    ConstantDataLists.homeCategoriesData[index]['image']!,
-                    fit: BoxFit.cover,
-                    height: 40,
-                    width: 40,
+          return GestureDetector(
+            onTap: () {
+              handleOnPressCategory(
+                  ConstantDataLists.homeCategoriesData[index]['title']!);
+            },
+            child: Column(
+              children: [
+                Container(
+                  padding: const EdgeInsets.symmetric(horizontal: 10),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(50),
+                    child: Image.asset(
+                      ConstantDataLists.homeCategoriesData[index]['image']!,
+                      fit: BoxFit.cover,
+                      height: 40,
+                      width: 40,
+                    ),
                   ),
                 ),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(top: 5),
-                child: Text(
-                  ConstantDataLists.homeCategoriesData[index]['title']!,
-                  style: const TextStyle(
-                    fontSize: GlobalVariables.textXS,
-                    fontWeight: FontWeight.w400,
+                Padding(
+                  padding: const EdgeInsets.only(top: 5),
+                  child: Text(
+                    ConstantDataLists.homeCategoriesData[index]['title']!,
+                    style: const TextStyle(
+                      fontSize: GlobalVariables.textXS,
+                      fontWeight: FontWeight.w400,
+                    ),
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           );
         },
       ),
