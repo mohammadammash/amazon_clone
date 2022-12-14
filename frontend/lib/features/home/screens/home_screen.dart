acd59d6ad3cada513ea8eaf45c5508966d825148
mohammadammash <mohammadammash31@gmail.com>
2022-12-14 21:35:56 +0200

    frontend/lib/features/home/screens/home_screen.dart: Adding onPressCategory function logic navigation

--- frontend/lib/features/home/screens/home_screen.dart
+++ frontend/lib/features/home/screens/home_screen.dart
@@ -1,13 +1,11 @@
-import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
+import 'package:amazon_clone/constants/routes_names.dart';
 import 'package:amazon_clone/features/common/widgets/app_bar.dart';
 import 'package:amazon_clone/features/home/widgets/address_box.dart';
 import 'package:amazon_clone/features/common/widgets/carousel_image.dart';
 import 'package:amazon_clone/features/home/widgets/deal_of_day.dart';
 import 'package:amazon_clone/features/home/widgets/top_categories.dart';
-import 'package:amazon_clone/providers/user_provider.dart';
 import 'package:amazon_clone/utils/authentication.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -19,6 +17,12 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
+    //Handle Navigate to specific category on click
+    void handleOnPressCategory(String title) {
+      Navigator.pushNamed(context, RoutesNames.categoryDetailsScreen,
+          arguments: title);
+    }
+
     Authentication authentication = Authentication();
     final currentUser = authentication.getCurrentUser(context: context);
 
@@ -34,7 +38,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 currentUserName: currentUser.name,
                 currentUserAddress: currentUser.address),
             const SizedBox(height: 10),
-            const TopCategories(),
+            TopCategories(handleOnPressCategory: handleOnPressCategory),
             const SizedBox(height: 10),
             const CarouselImage(),
             const DealOfDay(),
