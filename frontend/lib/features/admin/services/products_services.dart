08251113a1924eca8b1c808bbdcd2a4106726cd7
mohammadammash <mohammadammash31@gmail.com>
2022-12-14 11:44:06 +0200

    frontend/lib/features/admin/services/products_services.dart: Adding getAllProducts fetch

--- frontend/lib/features/admin/services/products_services.dart
+++ frontend/lib/features/admin/services/products_services.dart
@@ -1,14 +1,16 @@
+import 'dart:convert';
 import 'dart:io';
 import 'package:amazon_clone/constants/api_urls.dart';
 import 'package:amazon_clone/models/product.dart';
-import 'package:amazon_clone/providers/user_provider.dart';
+import 'package:amazon_clone/utils/authentication.dart';
 import 'package:amazon_clone/utils/response_handle.dart';
 import 'package:cloudinary_public/cloudinary_public.dart';
 import 'package:amazon_clone/utils/snackbar.dart';
 import 'package:flutter/material.dart';
 import 'package:amazon_clone/constants/config.dart';
 import 'package:http/http.dart' as http;
-import 'package:provider/provider.dart';
+
+final authentication = Authentication();
 
 class ProductsServices {
   void addProduct({
@@ -20,39 +22,37 @@ class ProductsServices {
     required String category,
     required List<File> images,
   }) async {
-    final String authToken =
-        Provider.of<UserProvider>(context, listen: false).user.token;
-
     try {
       //get my url images storage
-      final cloudinary = CloudinaryPublic(ConstantConfigData.cloudinaryName,
-          ConstantConfigData.cloudinaryUploadPreset);
-      List<String> imagesUrls = [];
+      // final cloudinary = CloudinaryPublic(ConstantConfigData.cloudinaryName,
+      //     ConstantConfigData.cloudinaryUploadPreset);
+      // List<String> imagesUrls = [];
 
-      for (int i = 0; i < images.length; i++) {
-        CloudinaryResponse res = await cloudinary.uploadFile(
-          CloudinaryFile.fromFile(images[i].path, folder: name),
-        );
-        imagesUrls.add(res.secureUrl);
-      }
+      // for (int i = 0; i < images.length; i++) {
+      //   CloudinaryResponse res = await cloudinary.uploadFile(
+      //     CloudinaryFile.fromFile(images[i].path, folder: name),
+      //   );
+      //   imagesUrls.add(res.secureUrl);
+      // }
 
       Product product = Product(
         name: name,
         description: description,
         quantity: quantity,
-        images: imagesUrls,
+        images: images,
         category: category,
         price: price,
       );
 
+      debugPrint('-------------');
+      debugPrint(product.toJson());
+      debugPrint('-------------');
+
       http.Response response = await http.post(
-          Uri.parse(ConstantApiUrls.addProductURL),
-          body: product.toJson(),
-          headers: <String, String>{
-            //to match express.json() in express server
-            'Content-Type': 'application/json; charset=UTF-8',
-            'x-auth-token': authToken,
-          });
+        Uri.parse(ConstantApiUrls.addProductURL),
+        body: product.toJson(),
+        headers: authentication.getAPIsHeader(context: context),
+      );
 
       httpResponseHandle(
         response: response,
@@ -63,7 +63,34 @@ class ProductsServices {
         }),
       );
     } catch (e) {
+      debugPrint(e.toString());
       showSnackBar(context, e.toString());
     }
+  }
+
+  Future<List<Product>> getAllProducts(BuildContext context) async {
+    List<Product> productList = [];
+    try {
+      http.Response response = await http.get(
+        Uri.parse(ConstantApiUrls.getAllProductsURL),
+        headers: authentication.getAPIsHeader(context: context),
+      );
+
+      httpResponseHandle(
+        response: response,
+        context: context,
+        onSuccess: () {
+          for (int i = 0; i < jsonDecode(response.body).length; i++) {
+            final jsonVal = jsonEncode(jsonDecode(response.body)[i]);
+
+            productList.add(Product.fromJson(jsonVal));
+          }
+        },
+      );
+    } catch (e) {
+      debugPrint(e.toString());
+      showSnackBar(context, e.toString());
+    }
+    return productList;
   }
 }
