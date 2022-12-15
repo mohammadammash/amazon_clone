c425b3f34e222564ecaa50580d90c8904931c740
mohammadammash <mohammadammash31@gmail.com>
2022-12-15 14:13:26 +0200

    backend/src/controllers/product.js: Adding search product controller

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -11,6 +11,18 @@ const getProductsByCategoryController = async (req, res) => {
   }
 };
 
+const getProductsSearchedFor = async (req, res) => {
+  try {
+    const {name} = req.params;
+    //if at least one matching chara found return product
+    const products = await Product.find({name: {$regex: name, $options: "i"}});
+    res.status(200).json(products);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+
 module.exports = {
   getProductsByCategoryController,
+  getProductsSearchedFor,
 };
