27d169dedafeaef1bbb87bea966d5000c4a9d00a
mohammadammash <mohammadammash31@gmail.com>
2022-12-15 14:15:16 +0200

    frontend/lib/features/search/screens/search_screen.dart: Adding intial state to fetch searched products

--- frontend/lib/features/search/screens/search_screen.dart
+++ frontend/lib/features/search/screens/search_screen.dart
@@ -1,3 +1,6 @@
+import 'package:amazon_clone/features/common/widgets/loading_indicator.dart';
+import 'package:amazon_clone/features/search/services/search_services.dart';
+import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
 class SearchProductsScreen extends StatefulWidget {
@@ -9,8 +12,28 @@ class SearchProductsScreen extends StatefulWidget {
 }
 
 class _SearchProductsScreenState extends State<SearchProductsScreen> {
+  List<Product>? productsList = [];a
+  final SearchServices searchServices = SearchServices();
+
+  void fetchProductsSearchedFor() async {
+    productsList = await searchServices.fetchSearchProducts(
+        context: context, searchText: widget.searchText);
+    setState(() {});
+    debugPrint('-------------------');
+    debugPrint(productsList!.length.toString());
+    debugPrint('-------------------');
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    fetchProductsSearchedFor();
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (productsList == null) return const CustomLoadingIndicator();
+
     return Scaffold(
         body: Center(
       child: Text(widget.searchText),
