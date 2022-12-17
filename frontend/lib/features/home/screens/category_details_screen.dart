0199a78d80003da5be10206cf4f6608f6e61c53e
mohammadammash <mohammadammash31@gmail.com>
2022-12-17 15:37:23 +0200

    frontend/lib/features/home/screens/category_details_screen.dart: Adding empty state

--- frontend/lib/features/home/screens/category_details_screen.dart
+++ frontend/lib/features/home/screens/category_details_screen.dart
@@ -62,62 +62,69 @@ class _CategoryDetailsScreenState extends State<CategoryDetailsScreen> {
               child: Text('Keep shopping for ${widget.category}',
                   style: const TextStyle(fontSize: GlobalVariables.textLG)),
             ),
-            SizedBox(
-              height: 170,
-              child: GridView.builder(
-                scrollDirection: Axis.horizontal,
-                padding: const EdgeInsets.only(left: 15),
-                itemCount: productsList!.length,
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 1,
-                  childAspectRatio: 1.4,
-                  mainAxisSpacing: 10,
-                ), //1 item at each row
-                itemBuilder: (context, index) {
-                  final shownProduct = productsList![index];
+            productsList!.isNotEmpty
+                ? SizedBox(
+                    height: 170,
+                    child: GridView.builder(
+                      scrollDirection: Axis.horizontal,
+                      padding: const EdgeInsets.only(left: 15),
+                      itemCount: productsList!.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 1,
+                        childAspectRatio: 1.4,
+                        mainAxisSpacing: 10,
+                      ), //1 item at each row
+                      itemBuilder: (context, index) {
+                        final shownProduct = productsList![index];
 
-                  return GestureDetector(
-                    onTap: () {
-                      handleOnPressItem(index);
-                    },
-                    child: Column(
-                      children: [
-                        //IMAGE BOX
-                        SizedBox(
-                          height: 130,
-                          child: DecoratedBox(
-                            decoration: BoxDecoration(
-                              border:
-                                  Border.all(color: Colors.black12, width: 0.5),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(10.0),
-                              child: Image.network(
-                                shownProduct.images[0],
+                        return GestureDetector(
+                          onTap: () {
+                            handleOnPressItem(index);
+                          },
+                          child: Column(
+                            children: [
+                              //IMAGE BOX
+                              SizedBox(
+                                height: 130,
+                                child: DecoratedBox(
+                                  decoration: BoxDecoration(
+                                    border: Border.all(
+                                        color: Colors.black12, width: 0.5),
+                                  ),
+                                  child: Padding(
+                                    padding: const EdgeInsets.all(10.0),
+                                    child: Image.network(
+                                      shownProduct.images[0],
+                                    ),
+                                  ),
+                                ),
                               ),
-                            ),
+                              //TEXT BOX
+                              Container(
+                                alignment: Alignment.center,
+                                padding: const EdgeInsets.only(
+                                  left: 0,
+                                  top: 5,
+                                  right: 15,
+                                ),
+                                child: Text(
+                                  shownProduct.name,
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                ),
+                              )
+                            ],
                           ),
-                        ),
-                        //TEXT BOX
-                        Container(
-                          alignment: Alignment.center,
-                          padding: const EdgeInsets.only(
-                            left: 0,
-                            top: 5,
-                            right: 15,
-                          ),
-                          child: Text(
-                            shownProduct.name,
-                            maxLines: 1,
-                            overflow: TextOverflow.ellipsis,
-                          ),
-                        )
-                      ],
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
-            )
+                  )
+                :
+                //EMPTY STATE
+                const Center(
+                    child: Text('No Items Found'),
+                  ),
           ],
         ));
   }
