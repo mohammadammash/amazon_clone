afa49de0b88a0e90b547a09a32f9571adf10ffc6
mohammadammash <mohammadammash31@gmail.com>
2022-12-17 19:46:43 +0200

    backend/src/routes/product.js: Fixing routes ordering bug

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -4,8 +4,8 @@ const router = express.Router();
 const { getProductsByCategoryController, getProductsSearchedFor, postRateProduct, getDealOfTheDayController } = require("../controllers/product");
 
 router.get("/", getProductsByCategoryController);
-router.get("/:name", getProductsSearchedFor);
 router.get("/deal_of_the_day", getDealOfTheDayController);
+router.get("/:name", getProductsSearchedFor);
 router.post("/rating", postRateProduct);
 
 module.exports = router;
