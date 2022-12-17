998db8c90c744ec702a2d6280826a221611cca33
mohammadammash <mohammadammash31@gmail.com>
2022-12-17 09:16:38 +0200

    frontend/lib/features/search/widgets/searched_product.dart: Finishing searched product shown data

--- frontend/lib/features/search/widgets/searched_product.dart
+++ frontend/lib/features/search/widgets/searched_product.dart
@@ -1,3 +1,5 @@
+import 'package:amazon_clone/constants/global_variables.dart';
+import 'package:amazon_clone/features/common/widgets/rating_stars.dart';
 import 'package:amazon_clone/models/product.dart';
 import 'package:flutter/material.dart';
 
@@ -12,23 +14,56 @@ class SearchedProduct extends StatelessWidget {
         Container(
           margin: const EdgeInsets.only(left: 3),
           child: Row(children: [
-            Image.network(
-              product.images[0],
-              fit: (BoxFit.fitHeight),
+            SizedBox(
+              width: MediaQuery.of(context).size.width / 3,
+              child: Image.network(
+                product.images[0],
+                fit: (BoxFit.fitHeight),
+              ),
             ),
-            Column(
-              children: [
-                Container(
-                    padding: const EdgeInsets.only(left: 10, top: 5),
-                    child: Text(
-                      product.name,
-                      style: const TextStyle(
-                        fontSize: 16,
-                      ),
-                      maxLines: 2,
-                    ))
-              ],
-            )
+            Padding(
+              padding: const EdgeInsets.only(left: 5),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    product.name,
+                    style: const TextStyle(
+                      fontSize: GlobalVariables.textSM,
+                    ),
+                    maxLines: 2,
+                  ),
+                  const SizedBox(height: 5),
+                  const StarsRating(rating: 4),
+                  const SizedBox(height: 5),
+                  Text(
+                    '\$${product.price}',
+                    style: const TextStyle(
+                      fontSize: GlobalVariables.textXL,
+                      fontWeight: FontWeight.w900,
+                    ),
+                    maxLines: 2,
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'Eligible for free shipping',
+                    style: TextStyle(
+                      fontSize: GlobalVariables.textXS,
+                    ),
+                    maxLines: 2,
+                  ),
+                  const SizedBox(height: 5),
+                  const Text(
+                    'In Stock',
+                    style: TextStyle(
+                      fontSize: GlobalVariables.textXXS,
+                      color: Colors.teal,
+                    ),
+                    maxLines: 2,
+                  ),
+                ],
+              ),
+            ),
           ]),
         ),
       ],
