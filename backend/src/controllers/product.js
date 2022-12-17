45a035a20f5ff8a58fc104b9a81015dd0012b04a
mohammadammash <mohammadammash31@gmail.com>
2022-12-17 19:47:15 +0200

    backend/src/controllers/product.js: Sorting and returnng op rated product

--- backend/src/controllers/product.js
+++ backend/src/controllers/product.js
@@ -43,7 +43,20 @@ const postRateProduct = async (req, res) => {
 
 const getDealOfTheDayController = async (req, res) => {
   try {
-    res.status(200).send({ msg: "DEALLL" });
+    const products = await Product.find({});
+
+    products.sort((a, b) => {
+      //sort desc order ratings
+      let aSum = 0;
+      let bSum = 0;
+
+      for (let i = 0; i < a.ratings.length; i++) aSum + a.ratings[i].rating;
+      for (let i = 0; i < b.ratings.length; i++) bSum + b.ratings[i].rating;
+
+      return aSum < bSum ? 1 : -1; //first < second then replace else not (aSum - bSum)
+    });
+
+    res.status(200).json(products[0]);
   } catch (e) {
     res.status(500).json({ error: e.message });
   }
