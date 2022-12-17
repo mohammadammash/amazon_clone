f1c4ec220739a6c60c0dfdd9f1680d41a64558e7
mohammadammash <mohammadammash31@gmail.com>
2022-12-17 16:02:22 +0200

    backend/src/routes/product.js: Adding get the deal of the day route

--- backend/src/routes/product.js
+++ backend/src/routes/product.js
@@ -1,10 +1,11 @@
 const express = require("express");
 const router = express.Router();
 //internal
-const { getProductsByCategoryController, getProductsSearchedFor, postRateProduct } = require("../controllers/product");
+const { getProductsByCategoryController, getProductsSearchedFor, postRateProduct, getDealOfTheDayController } = require("../controllers/product");
 
 router.get("/", getProductsByCategoryController);
 router.get("/:name", getProductsSearchedFor);
+router.get("/deal_of_the_day", getDealOfTheDayController);
 router.post("/rating", postRateProduct);
 
 module.exports = router;
