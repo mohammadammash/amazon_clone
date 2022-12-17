d9b18d32d57d26b9f1636d4760231cd8f23160d7
mohammadammash <mohammadammash31@gmail.com>
2022-12-17 09:36:33 +0200

    frontend/lib/utils/router.dart: Adding product screen to default router

--- frontend/lib/utils/router.dart
+++ frontend/lib/utils/router.dart
@@ -2,7 +2,9 @@ import 'package:amazon_clone/constants/routes_names.dart';
 import 'package:amazon_clone/features/admin/screens/add_product_screen.dart';
 import 'package:amazon_clone/features/auth/screens/auth_screen.dart';
 import 'package:amazon_clone/features/home/screens/category_details_screen.dart';
+import 'package:amazon_clone/features/product_details/screen/product_details_screen.dart';
 import 'package:amazon_clone/features/search/screens/search_screen.dart';
+import 'package:amazon_clone/models/product.dart';
 import "package:flutter/material.dart";
 
 Route<dynamic> generateRoute(RouteSettings settings) {
@@ -20,9 +22,17 @@ Route<dynamic> generateRoute(RouteSettings settings) {
       );
 
     case RoutesNames.searchProductsScreen:
-      final searchText = settings.arguments as String; //by def obj but we know we only will pass string to this route
+      final searchText = settings.arguments
+          as String; //by def obj but we know we only will pass string to this route
       return MaterialPageRoute(
         builder: (context) => SearchProductsScreen(searchText: searchText),
+      );
+
+    case RoutesNames.productDetailsScreen:
+      final product = settings.arguments
+          as Product;
+      return MaterialPageRoute(
+        builder: (context) => ProductDetailsScreen(product: product),
       );
 
     //ADMIN
