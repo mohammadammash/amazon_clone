b375648caadc7d3aba303f95233786d680c923ad
mohammadammash <mohammadammash31@gmail.com>
2022-12-19 18:13:43 +0200

    backend/src/controllers/user.js: Intializing basic add user address controller

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -54,8 +54,18 @@ const deleteProductFromCartController = async (req, res) => {
     res.status(500).json({ error: e.message });
   }
 };
+//-----------------------------------------------------
+
+const addCurrentUserAddressController = async (req, res) => {
+  try {
+    res.status(200).json({ msg: "address" });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
 
 module.exports = {
   addProductToCartController,
   deleteProductFromCartController,
+  addCurrentUserAddressController,
 };
