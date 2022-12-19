a585d9851b38817569f84af1522859ec7b203065
mohammadammash <mohammadammash31@gmail.com>
2022-12-19 09:43:30 +0200

    frontend/lib/features/user/cart/screens/address_screen.dart: Creating form and inputs controllers

--- frontend/lib/features/user/cart/screens/address_screen.dart
+++ frontend/lib/features/user/cart/screens/address_screen.dart
@@ -1,5 +1,7 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/common/widgets/app_bar.dart';
+import 'package:amazon_clone/features/common/widgets/button.dart';
+import 'package:amazon_clone/features/common/widgets/text_input.dart';
 import 'package:flutter/material.dart';
 
 class AddressScreen extends StatefulWidget {
@@ -10,13 +12,69 @@ class AddressScreen extends StatefulWidget {
 }
 
 class _AddressScreenState extends State<AddressScreen> {
+  final _addressFormKey = GlobalKey<FormState>();
+  final TextEditingController _flatBuildingController = TextEditingController();
+  final TextEditingController _areaController = TextEditingController();
+  final TextEditingController _pincodeController = TextEditingController();
+  final TextEditingController _cityController = TextEditingController();
+
+  @override
+  void dispose() {
+    super.dispose();
+    _flatBuildingController.dispose();
+    _areaController.dispose();
+    _pincodeController.dispose();
+    _cityController.dispose();
+  }
+
+  void handleContinueToPayment() {
+    debugPrint('-------------');
+    debugPrint('Continue to payment');
+    debugPrint('-------------');
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      appBar: PreferredSize(
+    return Scaffold(
+      appBar: const PreferredSize(
         preferredSize: Size.fromHeight(60),
         child: CustomAppBar(
             currentUserType: GlobalVariables.adminUserType, title: 'Address'),
+      ),
+      body: Form(
+        key: _addressFormKey,
+        child: Column(
+          children: [
+            CustomTextField(
+              controller: _flatBuildingController,
+              textPlaceholder: 'Flat, House Nb, Building',
+            ),
+            const SizedBox(height: 10),
+            CustomTextField(
+              controller: _areaController,
+              textPlaceholder: 'Area, Street',
+            ),
+            const SizedBox(height: 10),
+            CustomTextField(
+              controller: _pincodeController,
+              textPlaceholder: 'Pincode',
+            ),
+            const SizedBox(height: 10),
+            CustomTextField(
+              controller: _cityController,
+              textPlaceholder: 'Town/City',
+            ),
+            const SizedBox(height: 10),
+            CustomButton(
+              text: 'Pay on @',
+              handlePress: () => {
+                if (_addressFormKey.currentState!.validate())
+                  handleContinueToPayment()
+                //else it will show error validation for user automatically
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
