f87a3fd88ffe54204f0e73bd6861568bf8251e20
mohammadammash <mohammadammash31@gmail.com>
2022-12-19 09:14:56 +0200

    backend/src/controllers/user.js: Adding remove from cart controller

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -19,13 +19,36 @@ const addProductToCartController = async (req, res) => {
           break;
         }
       }
-
       if (!isFound) user.cart.push({ product, quantity: 1 });
     }
 
     const { _id, cart, ...rest } = user;
-    await User.findByIdAndUpdate(_id, { cart: cart }); //save user after updating cart
+    await User.findByIdAndUpdate(_id, { cart: cart }); //save user after updating //no need to await we have data already with user from auth
+    res.status(200).json(user);
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+};
+//-----------------------------------------------------------
 
+const deleteProductFromCartController = async (req, res) => {
+  try {
+    const { product_id } = req.body;
+    const { token, ...user } = req.user;
+
+    const product = await Product.findById(product_id);
+    for (let i = 0; i < user.cart.length; i++) {
+      //I used product._id because id from frontend is as String
+      //I used .equals because both are ObjectID
+      if (user.cart[i].product._id.equals(product._id)) {
+        if (user.cart[i].quantity <= 1) user.cart.splice(i, 1);
+        else user.cart[i].quantity--;
+        break;
+      }
+    }
+
+    const { _id, cart, ...rest } = user;
+    await User.findByIdAndUpdate(_id, { cart: cart }); //save user after updating
     res.status(200).json(user);
   } catch (e) {
     res.status(500).json({ error: e.message });
@@ -34,4 +57,5 @@ const addProductToCartController = async (req, res) => {
 
 module.exports = {
   addProductToCartController,
+  deleteProductFromCartController,
 };
