cdb43c19001d427a15bfe7bc245d22d04819f992
mohammadammash <mohammadammash31@gmail.com>
2022-12-19 08:32:49 +0200

    frontend/lib/features/user/cart/widgets/cart_product_quantity_input.dart: Adding onTap to increment cart item

--- frontend/lib/features/user/cart/widgets/cart_product_quantity_input.dart
+++ frontend/lib/features/user/cart/widgets/cart_product_quantity_input.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
 
 class CartProductQuantityInput extends StatefulWidget {
+  final String productId;
   final int quantity;
-  const CartProductQuantityInput({super.key, required this.quantity});
+  final Function(String) handlePressIncrementQuantity;
+  final Function(String) handlePressDecrementQuantity;
+  const CartProductQuantityInput({
+    super.key,
+    required this.productId,
+    required this.quantity,
+    required this.handlePressDecrementQuantity,
+    required this.handlePressIncrementQuantity,
+  });
 
   @override
   State<CartProductQuantityInput> createState() =>
@@ -19,30 +28,56 @@ class _CartProductQuantityInputState extends State<CartProductQuantityInput> {
         children: [
           Container(
               decoration: BoxDecoration(
+                color: Colors.grey.shade400,
                 border: Border.all(color: Colors.black, width: 1.5),
                 borderRadius: BorderRadius.circular(5),
               ),
               child: Row(children: [
+                //---------------
+                //DECREMENT ICON
                 Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1),
+                  ),
                   width: 30,
                   height: 30,
                   //Container > SizedBox to use 'Alignment'
                   alignment: Alignment.center,
                   child: const Icon(Icons.remove, size: 18),
                 ),
+                //---------------
+                //TOTAL QUANTITY
                 Container(
+                  decoration: BoxDecoration(
+                    border: Border.all(color: Colors.black, width: 1),
+                    color: Colors.white,
+                  ),
                   width: 30,
                   height: 30,
                   //Container > SizedBox to use 'Alignment'
                   alignment: Alignment.center,
-                  child: Text(widget.quantity.toString()),
+                  child: Text(
+                    widget.quantity.toString(),
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
                 ),
-                Container(
-                  width: 30,
-                  height: 30,
-                  //Container > SizedBox to use 'Alignment'
-                  alignment: Alignment.center,
-                  child: const Icon(Icons.add, size: 18),
+                //---------------
+                //INCREMENT ICON
+                InkWell(
+                  onTap: () =>
+                      widget.handlePressIncrementQuantity(widget.productId),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.black, width: 1),
+                    ),
+                    width: 30,
+                    height: 30,
+                    //Container > SizedBox to use 'Alignment'
+                    alignment: Alignment.center,
+                    child: const Icon(Icons.add, size: 18),
+                  ),
                 ),
               ]))
         ],
