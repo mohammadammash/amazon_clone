388d39f90b62671d95947b2fc5e4de54e8b3729c
mohammadammash <mohammadammash31@gmail.com>
2022-12-19 09:14:40 +0200

    backend/src/routes/user.js: Adding delete from cart route

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 //internal
-const { addProductToCartController } = require("../controllers/user");
+const { addProductToCartController, deleteProductFromCartController } = require("../controllers/user");
 
 router.post("/add_to_cart", addProductToCartController);
+router.delete("/delete_from_cart", deleteProductFromCartController);
 
 module.exports = router;
