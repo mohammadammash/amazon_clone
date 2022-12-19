079a47e2b69e4b89864aa6dd66302e358d07dc4c
mohammadammash <mohammadammash31@gmail.com>
2022-12-19 18:28:09 +0200

    backend/src/routes/user.js: Creating submit order route

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const router = express.Router();
 //internal
-const { addProductToCartController, deleteProductFromCartController, addCurrentUserAddressController } = require("../controllers/user");
+const {
+  addProductToCartController,
+  deleteProductFromCartController,
+  addCurrentUserAddressController,
+  postSubmitOrderController,
+} = require("../controllers/user");
 
 router.post("/add_to_cart", addProductToCartController);
 router.post("/address", addCurrentUserAddressController);
+router.post("/submit_order",postSubmitOrderController);
 router.delete("/delete_from_cart", deleteProductFromCartController);
 
 module.exports = router;
