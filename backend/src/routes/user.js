7caea4b4bbbd678fb07a6b671f023c3c9d6bcf22
mohammadammash <mohammadammash31@gmail.com>
2022-12-19 18:13:20 +0200

    backend/src/routes/user.js: Intializing addCurrentUserAddress route

--- backend/src/routes/user.js
+++ backend/src/routes/user.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
 //internal
-const { addProductToCartController, deleteProductFromCartController } = require("../controllers/user");
+const { addProductToCartController, deleteProductFromCartController, addCurrentUserAddressController } = require("../controllers/user");
 
 router.post("/add_to_cart", addProductToCartController);
+router.post("/address", addCurrentUserAddressController);
 router.delete("/delete_from_cart", deleteProductFromCartController);
 
 module.exports = router;
