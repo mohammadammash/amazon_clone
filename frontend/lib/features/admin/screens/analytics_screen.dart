a37ce2f7c3bb4de0a834229987deca390f7ae245
mohammadammash <mohammadammash31@gmail.com>
2022-12-20 12:05:58 +0200

    frontend/lib/features/admin/screens/analytics_screen.dart: Adding analytics chart widget to parent body

--- frontend/lib/features/admin/screens/analytics_screen.dart
+++ frontend/lib/features/admin/screens/analytics_screen.dart
@@ -1,7 +1,9 @@
 import 'package:amazon_clone/constants/global_variables.dart';
 import 'package:amazon_clone/features/admin/models/sales.dart';
 import 'package:amazon_clone/features/admin/services/analytics_services.dart';
+import 'package:amazon_clone/features/admin/widgets/category_stats_chart.dart';
 import 'package:amazon_clone/features/common/widgets/loading_indicator.dart';
+import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 
 class AnalyticsScreen extends StatefulWidget {
@@ -40,7 +42,18 @@ class _AnalyticsScreenState extends State<AnalyticsScreen> {
               style: const TextStyle(
                   fontSize: GlobalVariables.textLG,
                   fontWeight: FontWeight.bold),
-            )
+            ),
+            SizedBox(
+              height: 250,
+              child: CategoryStatsChart(seriesList: [
+                charts.Series(
+                  id: 'Sales',
+                  data: earnings!,
+                  domainFn: (Sales sales, _) => sales.label, //xAxis
+                  measureFn: (Sales sales, _) => sales.earnings, //yAxis
+                ),
+              ]),
+            ),
           ]);
   }
 }
