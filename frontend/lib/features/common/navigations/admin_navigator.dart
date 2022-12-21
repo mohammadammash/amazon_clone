3cee64a0759bab8c588d4ff4c7799c6ecb55e026
mohammadammash <mohammadammash31@gmail.com>
2022-12-21 17:53:22 +0200

    frontend/common/admin_navigator.dart: Replacing bottomNavBar with presitant_bottom_Bar

--- frontend/lib/features/common/navigations/admin_navigator.dart
+++ frontend/lib/features/common/navigations/admin_navigator.dart
@@ -2,10 +2,10 @@ import 'package:amazon_clone/features/admin/screens/all_orders_screen.dart';
 import 'package:amazon_clone/features/admin/screens/all_products_screen.dart';
 import 'package:amazon_clone/features/admin/screens/analytics_screen.dart';
 import 'package:amazon_clone/features/common/widgets/app_bar.dart';
-import 'package:amazon_clone/features/common/widgets/bottom_bar.dart';
-import 'package:amazon_clone/providers/user_provider.dart';
+import 'package:amazon_clone/utils/authentication.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
+import 'package:persistent_bottom_nav_bar/persistent_tab_view.dart';
 
 class AdminNavigator extends StatefulWidget {
   const AdminNavigator({super.key});
@@ -15,32 +15,82 @@ class AdminNavigator extends StatefulWidget {
 }
 
 class _AdminNavigatorState extends State<AdminNavigator> {
-  int page = 0;
+  Authentication authentication = Authentication();
 
-  List<Widget> pages = [
-    const AllProductsScreen(),
-    const AnalyticsScreen(),
-    const OrdersScreen(),
-  ];
+  final PersistentTabController _bottombarcontroller =
+      PersistentTabController(initialIndex: 0);
 
-  void updatePage(int newPageIndex) {
-    setState(() {
-      page = newPageIndex;
-    });
-  }
+  List<Widget> _buildScreens() => [
+        const AllProductsScreen(),
+        const AnalyticsScreen(),
+        const OrdersScreen(),
+      ];
+
+  List<PersistentBottomNavBarItem> _navBarsItems() => [
+        PersistentBottomNavBarItem(
+          icon: const Icon(CupertinoIcons.home),
+          title: ("Home"),
+          activeColorPrimary: CupertinoColors.activeGreen,
+          inactiveColorPrimary: CupertinoColors.systemGrey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(Icons.analytics),
+          title: ("Settings"),
+          activeColorPrimary: CupertinoColors.activeGreen,
+          inactiveColorPrimary: CupertinoColors.systemGrey,
+        ),
+        PersistentBottomNavBarItem(
+          icon: const Icon(CupertinoIcons.bag),
+          title: ("Settings"),
+          activeColorPrimary: CupertinoColors.activeGreen,
+          inactiveColorPrimary: CupertinoColors.systemGrey,
+        ),
+      ];
 
   @override
   Widget build(BuildContext context) {
-    final currentUser = Provider.of<UserProvider>(context).user;
+    final currentUser = authentication.getCurrentUser(context: context);
 
     return Scaffold(
       appBar: PreferredSize(
         preferredSize: const Size.fromHeight(60),
-        child: CustomAppBar(currentUserType: currentUser.type),
+        child: CustomAppBar(
+          currentUserType: currentUser.type,
+        ),
       ),
-      body: pages[page],
-      bottomNavigationBar:
-          CustomBottomBar(currentPage: page, updatePage: updatePage),
+      body: PersistentTabView(
+        context,
+        controller: _bottombarcontroller,
+        screens: _buildScreens(),
+        items: _navBarsItems(),
+        confineInSafeArea: true,
+        backgroundColor: Colors.white, // Default is Colors.white.
+        handleAndroidBackButtonPress: true, // Default is true.
+        resizeToAvoidBottomInset:
+            true, // This needs to be true if you want to move up the screen when keyboard appears. Default is true.
+        stateManagement: true, // Default is true.
+        hideNavigationBarWhenKeyboardShows:
+            true, // Recommended to set 'resizeToAvoidBottomInset' as true while using this argument. Default is true.
+        decoration: NavBarDecoration(
+          borderRadius: BorderRadius.circular(10.0),
+          colorBehindNavBar: Colors.white,
+        ),
+        popAllScreensOnTapOfSelectedTab: true,
+        popActionScreens: PopActionScreensType.all,
+        itemAnimationProperties: const ItemAnimationProperties(
+          // Navigation Bar's items animation properties.
+          duration: Duration(milliseconds: 200),
+          curve: Curves.ease,
+        ),
+        screenTransitionAnimation: const ScreenTransitionAnimation(
+          // Screen transition animation on change of selected tab.
+          animateTabTransition: true,
+          curve: Curves.ease,
+          duration: Duration(milliseconds: 200),
+        ),
+        navBarStyle:
+            NavBarStyle.style1, // Choose the nav bar style with this property.
+      ),
     );
   }
 }
